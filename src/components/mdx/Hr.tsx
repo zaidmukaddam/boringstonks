dc7477b9c8d4b504d2b7815d8671d73bc39d1d4f
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2022-04-15 16:43:55 +0530

    Making it more visible

--- src/components/mdx/Hr.tsx
+++ src/components/mdx/Hr.tsx
@@ -2,7 +2,7 @@ import Decoration from '../vectors/Decoration'
 
 const Hr = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div style={{ display:"grid", justifyContent: "center", alignContent: "center" }}>
+        <div style={{ display: "grid", justifyContent: "center", alignContent: "center", margin: "4rem" }}>
             <Decoration />
             {children}
         </div>
